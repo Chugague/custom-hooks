db87c1d245edb3ff99ca8a3bb48fdc68dcaf2867
Kevin Ugalde <kevin.ugalde.g@gmail.com>
2024-02-15 17:24:02 -0300

    primer commit

---
+++ useTodos.js
@@ -0,0 +1,62 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+
+const initialState = []
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodos = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
+    
+    const handleNewTodo = (todo) => {
+
+        const action = {
+            type: 'add Todo',
+            payload: todo
+        }
+
+        dispatch(action);
+    }
+
+    const handleDelete = (id) => {
+
+        const action = {
+            type: 'delete Todo',
+            payload: id
+        }
+
+        dispatch(action);
+    }
+
+    const handleToggle = (id) => {
+            dispatch({
+                type: 'toggle Todo',
+                payload: id
+            });
+        }
+
+    const todosCount = todos.length;
+    const pendingTodosCount = todos.filter(todo => !todo.done).length;
+
+
+        return {
+            todosCount,
+            pendingTodosCount,
+            todos,
+            handleNewTodo,
+            handleDelete,
+            handleToggle
+        }
+
+
+}
